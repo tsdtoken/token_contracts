20b55738d7bec00c9f5b70cca1617ebb26137192
Venkatesh Namburi <vnamburi92@gmail.com>
2018-07-04 15:33:21 +1000

    accomodate for price variation edge case

--- contracts/TSDCrowdSale.sol
+++ contracts/TSDCrowdSale.sol
@@ -96,9 +96,8 @@ contract TSDCrowdSale is Ownable {
         uint256 currentRate = exchangeRate;
         uint256 oneSzabo = 1 szabo;
         // 1 ETH = 1000000 szabo
-        uint256 tokenPriceInSzabo = tokenPrice.mul(1000000).div(_newRate);
         // The exchangerate is saved in Szabo.
-        exchangeRate = oneSzabo.mul(tokenPriceInSzabo);
+        exchangeRate = oneSzabo.mul(tokenPrice).mul(1000000).div(_newRate);
         emit ExchangeRateUpdated(currentRate, exchangeRate);
         return true;
     }
